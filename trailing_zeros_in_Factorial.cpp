e07ef68a55beb0cf922fc843403728f797edc717
Harsh <82045730+Harsh836-ui@users.noreply.github.com>
2023-06-15 12:10:13 +0530

    Update trailing_zeros_in_Factorial.cpp

--- trailing_zeros_in_Factorial.cpp
+++ trailing_zeros_in_Factorial.cpp
@@ -2,23 +2,22 @@
 using namespace std;
 typedef long long int lli;
 
+CountTZ(int n ) {
+	int res = 0;
+	for (int i = 5; i <= n; i *= 5)
+	{
+		res = res + (n / i);
+	}
+	return res;
+
+}
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n; cin >> n;
-//This code will give factorial of given num
-	int res = 1;
-	for (int i = 2; i <= n; ++i)
-	{
-		res = res * i;
-	}
-	cout << res;
 
-	/*int count = 0;
-	while (res % 10 == 0) {
-		count++;
-	}
-	cout << count;*/
+	int n; cin >> n;
+	cout << CountTZ(n);
+
 
 	return 0;
 }
